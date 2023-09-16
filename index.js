3b024e427e1cebe6f3ed8eb874b6ca678f02be9b
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-16 14:27:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const app = require("./src/app");
+const db = require("./src/db");
+
+const PORT = process.env.PORT || 3001;
+
+db();
+app.listen(PORT, () => {
+  console.log(`Server on port:${PORT}`);
+});
