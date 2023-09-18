b776fbddb86e2a12522dabb1bb699fcc9c8a008c
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-18 14:31:44 -0300

    fix: db uri

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@maincluster.0yhxkwz.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.0yhxkwz.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {
