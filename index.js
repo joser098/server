ff4d9cf60caf3151a3a31854db7aa283bd8d1abb
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-19 16:15:14 -0300

    feat: middleware added

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 require("dotenv").config();
 const app = require("./src/app");
 const db = require("./src/db");
+const PORT = process.env.PORT ?? 3001;
 
-const PORT = process.env.PORT || 3001;
-
-db();
+// db();
 app.listen(PORT, () => {
   console.log(`Server on port:${PORT}`);
 });
--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+const whitelist = ["http://localhost:5173", "jxsejaramillo.com"];
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (whitelist.includes(origin) || !origin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
+
+app.disable("x-powered-by");
 
 module.exports = app;
