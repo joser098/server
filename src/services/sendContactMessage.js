9c81fad7b353cb143dc8ab3ea6283e36c58bb930
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-10-17 17:18:19 -0300

    feat: created && added emailHtml

--- src/services/sendContactMessage.js
+++ src/services/sendContactMessage.js
@@ -1,14 +1,19 @@
 const { Resend } = require("resend");
+const path = require("path");
+const fs = require("fs");
 
 const sendContactMessage = async (email) => {
   try {
     const resend = new Resend(process.env.RESEND_API_KEY);
 
+    const htmlPath = path.join(__dirname, "..", "public", "contactEmail.html");
+    const emailHtml = fs.readFileSync(htmlPath, "utf-8");
+
     const data = await resend.emails.send({
       from: process.env.RESEND_EMAIL,
       to: email,
-      subject: "Gracias por tu mensaje de contacto",
-      html: "<h2>Gracias por tu mensaje, pronto recibirás más información.</h2>",
+      subject: "Gracias por tu mensaje",
+      html: emailHtml,
     });
 
     return data;
