8a9755904744dc12fcefa1a6fc9a896dbec09b26
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-10-24 16:54:08 -0300

    feat: new endpoit to save survey results

--- src/routes/jxseRoutes/jxseRoutes.js
+++ src/routes/jxseRoutes/jxseRoutes.js
@@ -1,11 +1,13 @@
 const getProfile = require("../../handlers/jxseHandlers/getProfile");
 const getProjects = require("../../handlers/jxseHandlers/getProjects");
 const postMessage = require("../../handlers/jxseHandlers/postMessage");
+const postSurveyResult = require("../../handlers/jxseHandlers/postSurveyResult");
 
 const jxseRouter = require("express").Router();
 
 jxseRouter.get("/profile", getProfile);
 jxseRouter.get("/projects", getProjects);
 jxseRouter.post("/message", postMessage);
+jxseRouter.post("/survey", postSurveyResult);
 
 module.exports = jxseRouter;
